ab67078c7ac0977d0769b5bb5a504fb87069d406
quangday123 <quangpmhe176852@fpt.edu.vn>
2025-02-16 23:48:01 +0700

    inventory

---
+++ Assets/Script/UI/Inventory/InventoryData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script.UI.Inventory
+{
+    [System.Serializable]
+    public class InventoryData
+    {
+        public List<ItemData> items = new List<ItemData>();
+    }
+
+    [System.Serializable]
+    public class ItemData
+    {
+        public string itemName;
+        public int slotIndex;
+    }
+
+
+}

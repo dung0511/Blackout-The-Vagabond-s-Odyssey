0d49b68936ef5eb19addd4ccb0d2b1e4e1c468f1
LongNoobb <longtdhe172725@fpt.edu.vn>
2025-02-28 09:48:35 +0700

    add variant ranged weapon

--- Assets/Script/Weapon/WeaponController.cs
+++ Assets/Script/Weapon/WeaponController.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     
-    public int currentWeaponIndex=1;
+    public int currentWeaponIndex=0;
     
     public GameObject Weapon1;
     public GameObject Weapon2;
+    public List<GameObject> obj;
    // public RangedWeapon rangedWeapon;
    // public MeleeWeapon meleeWeapon;
     void Start()
     {
+        obj = new List<GameObject>();
+        obj = GetAllChild();
+        foreach(GameObject obj in GetAllChild())
+        {
+            obj.SetActive(false);
+        }
         Weapon1 = GetAllChild()[0];
         Weapon2 = GetAllChild()[1];
         Weapon1.active = true;
@@ -26,13 +33,13 @@ public class WeaponController : MonoBehaviour
             case 1:
                 Weapon1.active = true;
                 Weapon2.active = false;
-              
+                currentWeaponIndex = 0;
                 break;
 
             case 2:
                 Weapon1.active = false;
                 Weapon2.active = true;
-               
+                currentWeaponIndex = 1;
                 break;
         }
     }

e44065f67cd828d510dc2b8a1efb739ebbf348de
quangday123 <quangpmhe176852@fpt.edu.vn>
2025-03-02 01:33:50 +0700

    add hotbar

---
+++ Assets/Script/UI/Inventory/Item.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Script.UI.Inventory
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Item")]
+
+    public class Item : ScriptableObject
+    {
+        [Header("Only UI")]
+        public bool stackable = true;
+
+        [Header("Only Gameplay")]
+        public ItemType type;
+
+        // Common properties for all items
+        public string itemName;
+        public Sprite icon;
+    }
+
+    public enum ItemType
+    {
+        Equipment,
+        Weapon,
+        Potion
+    }
+
+    public enum ActionType
+    {
+        Attack,
+        Equip,
+        Use
+    }
+
+    // Subclass for Weapon
+    [CreateAssetMenu(menuName = "Scriptable object/Weapon")]
+    public class Weapon : Item
+    {
+        public int damage;
+        public float attackRange;
+
+        public Weapon()
+        {
+            type = ItemType.Weapon;
+            stackable = false; // Weapons are usually not stackable
+        }
+    }
+
+    // Subclass for Potion
+    [CreateAssetMenu(menuName = "Scriptable object/Potion")]
+    public class Potion : Item
+    {
+        public int regen;
+
+        public Potion()
+        {
+            type = ItemType.Potion;
+            stackable = true; // Potions are usually stackable
+        }
+    }
+}

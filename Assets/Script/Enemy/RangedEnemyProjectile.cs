7daea7d4c157bd9e2a033a8b36a8dee1044e7c38
LongNoobb <longtdhe172725@fpt.edu.vn>
2025-03-11 04:58:26 +0700

    add ranged enemy

---
+++ Assets/Script/Enemy/RangedEnemyProjectile.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Script.Boss.StoneGolem;
+using System.Collections;
+using UnityEngine;
+
+public class RangedEnemyProjectile : MonoBehaviour
+{
+    public Transform target;
+    public GameObject projectile;
+    public Transform firePos;
+    public float projectileForce;
+    public int projectileDame;
+
+    public RangedWeaponSO rangedDetail;
+
+    public Enemy enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        projectileForce = rangedDetail.bulletForce;
+        projectileDame = rangedDetail.damageRangedWeapon;
+    }
+
+    void Fire()
+    {
+
+        GameObject bulletTmp = BulletPoolManagement.Instance.GetBullet(projectile);
+        if (bulletTmp == null) return;
+
+        bulletTmp.transform.position = firePos.position;
+        Vector2 direction = (Vector2)(target.position - firePos.position);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        bulletTmp.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        Rigidbody2D rb = bulletTmp.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = Vector2.zero;
+
+        Vector2 shootDirection = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        rb.AddForce(shootDirection * projectileForce, ForceMode2D.Impulse);
+    }
+
+}

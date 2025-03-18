7165723b6c377b48fd2795090cd2f278ee1b1b87
quangday123 <quangpmhe176852@fpt.edu.vn>
2025-03-19 02:55:10 +0700

    add stackable, pick up, swap items for inventory
    đã xong stack items , nhặt items, swap items

--- Assets/Script/UI/Controller/InventoryController.cs
+++ Assets/Script/UI/Controller/InventoryController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
@@ -5,11 +7,79 @@ public class InventoryController : MonoBehaviour
     [SerializeField]
     private UIInventory inventoryUI;
 
-    public int inventorySize = 10;
+    [SerializeField]
+    private InventorySO inventoryData;
+
+    public List<InventoryItem> initialItems = new List<InventoryItem>();
 
     private void Start()
     {
-        inventoryUI.InitializeInventoryUI(inventorySize);
+        PrepareUI();
+        PrepareInventoryData();
+    }
+
+    private void PrepareInventoryData()
+    {
+        inventoryData.Initialize();
+        inventoryData.OnInventoryChanged += UpdateInventoryUI;
+        foreach (InventoryItem item in initialItems)
+        {
+            if (item.isEmpty)
+                continue;
+            inventoryData.AddItem(item);
+
+        }
+    }
+
+    private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
+    {
+        inventoryUI.ResetAllItems();
+        foreach (var item in inventoryState)
+        {
+            inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage,
+                item.Value.quantity);
+        }
+    }
+
+    private void PrepareUI()
+    {
+        inventoryUI.InitializeInventoryUI(inventoryData.size);
+        this.inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+        this.inventoryUI.OnSwapItems += HandleSwapItems;
+        this.inventoryUI.OnStartDragging += HandleDragging;
+        this.inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+    }
+
+    private void HandleItemActionRequest(int itemIndex)
+    {
+
+    }
+
+    private void HandleDragging(int itemIndex)
+    {
+        InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+        if (inventoryItem.isEmpty)
+            return;
+        inventoryUI.CreateDraggedItem(inventoryItem.item.ItemImage, inventoryItem.quantity);
+    }
+
+    private void HandleSwapItems(int itemIndex1, int itemIndex2)
+    {
+        inventoryData.SwapItems(itemIndex1, itemIndex2);
+    }
+
+    private void HandleDescriptionRequest(int itemIndex)
+    {
+        InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+        if (inventoryItem.isEmpty)
+        {
+            inventoryUI.ResetSelection();
+            return;
+        }
+
+        ItemSO item = inventoryItem.item;
+        inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+            item.name, item.Description);
     }
 
     private void Update()
@@ -19,6 +89,12 @@ public class InventoryController : MonoBehaviour
             if (inventoryUI.isActiveAndEnabled == false)
             {
                 inventoryUI.Show();
+                foreach (var item in inventoryData.GetCurrentState())
+                {
+                    inventoryUI.UpdateData(item.Key,
+                        item.Value.item.ItemImage,
+                        item.Value.quantity);
+                }
             }
             else
             {
